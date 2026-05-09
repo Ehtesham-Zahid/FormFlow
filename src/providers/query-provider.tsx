7bd85a897b5c37685f1f2b84f7e291000b3ba265
Ehtesham Zahid <ehteshamzahid313@gmail.com>
2026-05-09 12:28:57 +0500

    Query Provider Done

---
+++ src/providers/query-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function QueryProvider({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
